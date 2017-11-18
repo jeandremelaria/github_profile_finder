3e3785016c38386462383e7759aaea6acdf5b7cf
Jeandre Melaria <J.melaria@hotmail.com>
2017-11-18 15:08:36 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+//----- CONSTANTS -----//
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const browserSync = require('browser-sync');
+const htmlmin = require('gulp-htmlmin');
+
+
+//----- LOG MESSAGE -----//
+gulp.task('message', function(){
+	return console.log('Gulp is running...');
+}); // To run type 'gulp message' in command line
+
+//----- COPY ALL HTML FILES TO BUILD FOLDER AD MINIFY -----//
+gulp.task('copyHtml', function(){
+	gulp.src('src/*.html')
+	 .pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest('build'));
+});
+
+//----- GULP PREFIXER AND CSS NANO -----//
+gulp.task('prefixerAndCssNano', function(){
+    gulp.src('src/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+		.pipe(cssnano())
+        .pipe(gulp.dest('build/css'))
+});
+
+//----- BROWSER SYNC -----//
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'build' //build folder
+        }
+    });
+});
+
+//----- RUN MULTIPLE TASKS AT ONCE IN AN ARRAY -----//
+gulp.task('default', ['message', 'copyHtml','prefixerAndCssNano']);
+
+//----- GULP WATCH -----//
+gulp.task('watch',['browserSync'], function(){
+	gulp.watch('src/css/*.css',['prefixerAndCssNano']);
+	gulp.watch('src/*.html',['copyHtml']);
+	
+	//reload browser when the files change
+	gulp.watch('build/css/*.css', browserSync.reload);
+	gulp.watch('build/*.html', browserSync.reload);
+ 
+});
+ 
